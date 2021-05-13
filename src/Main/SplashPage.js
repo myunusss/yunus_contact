40dfcc30bcf79959aea1252d34091c7c130f4c12
Muhammad Yunus <turmudi.yunus@gmail.com>
2021-05-13 20:57:52 +0700

    fixing style

--- src/Main/SplashPage.js
+++ src/Main/SplashPage.js
@@ -1,40 +1,38 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from "react-native";
-import { useEffect } from 'react';
-import { Actions } from 'react-native-router-flux';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {useEffect} from 'react';
+import {Actions} from 'react-native-router-flux';
 
 const SplashPage = () => {
   // COMPONENTDIDMOUNT
   useEffect(() => {
     setTimeout(() => {
-      Actions.reset('home')
+      Actions.reset('home');
     }, 1000);
-  }, [])
-  
+  }, []);
+
   //COMPONENTDIDUPDATE
   useEffect(() => {
     // return value
-  })
+  });
 
   //COMPONENTWILLUNMOUNT
   useEffect(() => {
-    return() => {
+    return () => {
       // value
-    }
-  }, [])
+    };
+  }, []);
 
-  return(
+  return (
     <View style={styles.container}>
       <Image
         source={require('../Shared/assets/images/splashscreen.png')}
         style={{width: 200, height: 200, resizeMode: 'cover'}}
       />
-      <Text style={styles.versionText}>
-        v 0.0.1 - 2021
-      </Text>
+      <Text style={styles.versionText}>v 0.0.1 - 2021</Text>
     </View>
-  )
-}
+  );
+};
 
 export default SplashPage;
 
@@ -42,11 +40,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   versionText: {
     position: 'absolute',
     bottom: 40,
-    alignSelf: 'center'
-  }
-})
+    alignSelf: 'center',
+  },
+});
